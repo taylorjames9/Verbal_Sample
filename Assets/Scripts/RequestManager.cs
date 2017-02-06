a7eb41ba6b5e52f61079c6ee36ba785a4e10392a
taylorjames9 <taylorjames9@gmail.com>
2017-02-05 23:11:05 -0800

    all keys recognizable

--- Assets/Scripts/RequestManager.cs
+++ Assets/Scripts/RequestManager.cs
@@ -10,11 +10,25 @@ public class RequestManager : MonoBehaviour {
 	void Update(){
         if(Input.GetKeyUp(KeyCode.L)){
             Debug.Log("L Key pressed");
-            //Start Paddle Rotation
             StartCoroutine(Pivot.Instance.Scan("L"));
             //Paddle should update its hitTarget depending on which location is sought
             //OnRequestMade("Lobby");
             Paddle.Instance.ChooseTarget("L");
+        }
+        if(Input.GetKeyUp(KeyCode.E)){
+            Debug.Log("E Key pressed");
+            StartCoroutine(Pivot.Instance.Scan("E"));
+            Paddle.Instance.ChooseTarget("E");
+        }
+        if(Input.GetKeyUp(KeyCode.R)){
+            Debug.Log("R Key pressed");
+            StartCoroutine(Pivot.Instance.Scan("R"));
+            Paddle.Instance.ChooseTarget("R");
+        }
+        if(Input.GetKeyUp(KeyCode.B)){
+            Debug.Log("B Key pressed");
+            StartCoroutine(Pivot.Instance.Scan("B"));
+            Paddle.Instance.ChooseTarget("B");
         }
 	}
 
